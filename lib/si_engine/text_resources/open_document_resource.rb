c68d88c938ce9522a5e1598cf81c6fe19b602d81
Michael Price <mike@mightystrongmedia.com>
2015-10-14 14:12:19 -0600

    Added an empty OpenDocumentResource class.

---
+++ lib/si_engine/text_resources/open_document_resource.rb
@@ -0,0 +1,12 @@
+# This class was originally included in one file; see src/part1/text-resource.rb
+# The various classes have been broken down into separate files for readability.
+# Classes: [TextResource, PlainTextResource, BinararyPlainTextResource,
+# =>        HtmlXhtmlResource, OpenDocumentResource, RssResource, AtomResource,
+# =>        ClassifierAndSummarization, ClassifierWordCountStatistics,
+# =>        ClassifierWordCountStatistics, SentimentOfText, EntityExtraction]
+
+module SiEngine
+  class OpenDocumentResource < TextResource
+    
+  end
+end
